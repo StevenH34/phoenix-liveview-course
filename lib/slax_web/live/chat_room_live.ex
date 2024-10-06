1de8111ae453fafd7e73c1bbba142673a0465e25
Steven Hedges <steven.hedges34@gmail.com>
2024-10-06 19:47:25 -0400

    Add rooms to sidebar

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -7,6 +7,23 @@ defmodule SlaxWeb.ChatRoomLive do
 
   def render(assigns) do
     ~H"""
+    <div class="flex flex-col flex-shrink-0 w-64 bg-slate-100">
+      <div class="flex justify-between items-center flex-shrink-0 h-16 border-b border-slate-300 px-4">
+        <div class="flex flex-col gap-1.5">
+          <h1 class="text-lg font-bold text-gray-800">
+            Slax
+          </h1>
+        </div>
+      </div>
+      <div class="mt-4 overflow-auto">
+        <div class="flex items-center h-8 px-3 group">
+          <span class="ml-2 leading-none font-medium text-sm">Rooms</span>
+        </div>
+        <div id="rooms-list">
+          <.room_link :for={room <- @rooms} room={room} active={room.id == @room.id} />
+        </div>
+      </div>
+    </div>
     <div class="flex flex-col flex-grow shadow-lg">
       <div class="flex justify-between items-center flex-shrink-0 h-16 bg-white border-b border-slate-300 px-4">
         <div class="flex flex-col gap-1.5">
@@ -26,19 +43,40 @@ defmodule SlaxWeb.ChatRoomLive do
     """
   end
 
+  attr :active, :boolean, required: true
+  attr :room, Room, required: true
+
+  defp room_link(assigns) do
+    ~H"""
+    <a
+      class={[
+        "flex items-center h-8 text-sm pl-8 pr-3",
+        (@active && "bg-slate-300") || "hover:bg-slate-300"
+      ]}
+      href="#"
+    >
+      <.icon name="hero-hashtag" class="h-4 w-4" />
+      <span class={["ml-2 leading-none", @active && "font-bold"]}>
+        <%= @room.name %>
+      </span>
+    </a>
+    """
+  end
+
   def mount(_params, _session, socket) do
     # mount must return a tuple with the fist arg being :ok
     # second elem is the socket. An instance of %Phoenix.LiveView.Socket{}.
-    # {:ok, socket}
 
     # Loading %Room{}
-    room = Room |> Repo.all() |> List.first()
+    rooms = Repo.all(Room)
+    room = List.first(rooms)
+
     # socket.assigns is a map of assigns
     # This gets passed to render/1 as assigns
-    {:ok, assign(socket, hide_topic?: false, room: room)}
+    {:ok, assign(socket, hide_topic?: false, room: room, rooms: rooms)}
   end
 
   def handle_event("toggle-topic", _params, socket) do
-    {:noreply, assign(socket, hide_topic?: !socket.assigns.hide_topic?)}
+    {:noreply, update(socket, :hide_topic?, &(!&1))}
   end
 end
