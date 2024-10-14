bca2ad81d42b0f745373712f6fa7baed3f901e9c
Steven Hedges <steven.hedges34@gmail.com>
2024-10-14 10:17:38 -0400

    convert ChatRoomLive to use stream

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -38,14 +38,11 @@ boromir = Accounts.get_user_by_email("boromir@fellowship.me")
 room = Repo.insert!(%Room{name: "council-of-elrond", topic: "What to do with the ring?"})
 
 for {user, message} <- [
-    {elrond,
-    "Strangers from distant lands, friends of old. You have been summoned here to answer the threat of Mordor. Middle-Earth stands upon the brink of destruction. None can escape it. You will unite or you will fall. Each race is bound to this fate–this one doom."},
+    {elrond, "Strangers from distant lands, friends of old. You have been summoned here to answer the threat of Mordor. Middle-Earth stands upon the brink of destruction. None can escape it. You will unite or you will fall. Each race is bound to this fate–this one doom."},
     {elrond, "Bring forth the Ring, Frodo."},
     {boromir, "So it is true…"},
-    {boromir,
-    "It is a gift. A gift to the foes of Mordor. Why not use this Ring? Long has my father, the Steward of Gondor, kept the forces of Mordor at bay. By the blood of our people are your lands kept safe! Give Gondor the weapon of the Enemy. Let us use it against him!"},
-    {aragorn,
-    "You cannot wield it! None of us can. The One Ring answers to Sauron alone. It has no other master."},
+    {boromir, "It is a gift. A gift to the foes of Mordor. Why not use this Ring? Long has my father, the Steward of Gondor, kept the forces of Mordor at bay. By the blood of our people are your lands kept safe! Give Gondor the weapon of the Enemy. Let us use it against him!"},
+    {aragorn, "You cannot wield it! None of us can. The One Ring answers to Sauron alone. It has no other master."},
     {boromir, "And what would a ranger know of this matter?"}
     ] do
   Repo.insert!(%Message{user: user, room: room, body: message})
